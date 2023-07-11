d553284ee2af2bbc87be9e20d41db3ae9541c253
Arman Zhalgasbayev <108217670+silvermete0r@users.noreply.github.com>
2023-07-11 16:56:07 +0600

    Update time_to_count_1_billion.py

--- time_to_count_1_billion.py
+++ time_to_count_1_billion.py
@@ -9,3 +9,9 @@ for i in range(1000000000):
 end = time.time()
 
 print(end - start, 'sec')
+
+# Results on my PC: 73.83807063102722 sec
+# My PC Characteristics:
+# CPU: Intel Core I5 11th Gen (2.40 GHz)
+# RAM: 8 GB
+# OS: Windows 10 - 64x
